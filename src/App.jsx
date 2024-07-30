c79b6b642ea6593a363109bda7365d3cf463ee4d
Isaac1676 <ezechielncho225@gmail.com>
2024-07-30 11:53:39 +0000

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
@@ -6,22 +7,27 @@ import BudgetPage from './components/BudgetPage/BudgetPage';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import Register from './components/Register/Register';
+import Footer from './components/Footer/Footer';
+import './App.css';
 
 const App = () => {
     const user = JSON.parse(localStorage.getItem('user'));
 
     return (
-        <>
-            <Nav />
-            <Router>
-                <Routes>
-                    <Route path="/" element={user ? <Navigate to={`/h/${user.id}`} /> : <Register />} />
-                    <Route path="/h/:userId" element={<Home />} />
-                    <Route path="/h/:userId/budgets/:budgetID" element={<BudgetPage />} />
-                </Routes>
-            </Router>
-            <ToastContainer />
-        </>
+        <Router>
+            <div className="app-container">
+                <Nav />
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/" element={user ? <Navigate to={`/h/${user.id}`} /> : <Register />} />
+                        <Route path="/h/:userId" element={<Home />} />
+                        <Route path="/h/:userId/budgets/:budgetID" element={<BudgetPage />} />
+                    </Routes>
+                </main>
+                <Footer />
+                <ToastContainer />
+            </div>
+        </Router>
     );
 };
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='copyright'>&copy; 2024 Isaac N'cho. Tous droits réservés.</div>
+        </footer>
+    )
+}
+
+export default Footer
